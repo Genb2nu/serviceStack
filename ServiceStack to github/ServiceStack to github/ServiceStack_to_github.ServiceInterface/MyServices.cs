aced70ff78bdd16ac061ae6c0ee3e9e72861c0c6
Eugene Van Betonio <betonio.eugene@gmail.com>
2016-03-10 15:28:39 +0800

    BaseCode

---
+++ ServiceStack to github/ServiceStack to github/ServiceStack_to_github.ServiceInterface/MyServices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+using ServiceStack_to_github.ServiceModel;
+
+namespace ServiceStack_to_github.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = "Hello, {0}!".Fmt(request.Name) };
+        }
+
+        public object Any(NameRequest request)
+        {
+            return new NameResponse { Result = "Name, {0}!".Fmt(request.Name) };
+        }
+
+    }
+}
---
+++ ServiceStack to github/ServiceStack to github/ServiceStack_to_github/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServiceStack_to_github.Global" Language="C#" %>
